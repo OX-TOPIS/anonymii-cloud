424f972f66b5e88bb438d88a30bbf72631f449b2
OX-TOPIS <anthicha234@gmail.com>
2024-02-04 13:21:33 +0700

    add

--- src/index.js
+++ src/index.js
@@ -3,11 +3,60 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import Channel from "./pages/chat/Channel.jsx";
+import MyChannel from "./pages/MyChannel.jsx";
+import Notifications from "./pages/Notifications.jsx"
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/mychannel",
+        element: <MyChannel />,
+      },
+      {
+        path: "/channel",
+        element: <Channel />,
+      },
+      {
+        path: "/notifications",
+        element: <Notifications />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
