23d58f6dc7af6c37bf23d635752ffb6a257daba8
baebey <64070234@kmitl.ac.th>
2024-02-06 18:07:16 +0700

    profile success

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,6 +9,8 @@ const Profile = () => {
   const [refreshUsername, setRefreshUsername] = useState("");
   const [refreshPassword, setRefreshPassword] = useState("");
   const [refreshConfirmPassword, setRefreshConfirmPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
 
   useEffect(() => {
@@ -59,6 +61,14 @@ const Profile = () => {
       setRefreshPassword("");
       setRefreshConfirmPassword("");
     }
+  }
+
+
+  function togglePasswordVisibility() {
+    setIsPasswordVisible((prevState) => !prevState);
+  }
+  function toggleConfirmPasswordVisibility() {
+    setIsConfirmPasswordVisible((prevState) => !prevState);
   }
 
   return (
@@ -153,7 +163,7 @@ const Profile = () => {
           <h3 className="font-bold text-white mb-3">Change your Username</h3>
           <input
               type="text"
-              className="mb-2 w-72 outline-blue2 border-blue2 rounded-md p-2 bg-gray-100 border-2 placeholder-blue2 text-blue2 font-bold"
+              className="mb-2 w-full outline-blue2 border-blue2 rounded-md p-2 bg-gray-100 border-2 placeholder-blue2 text-blue2 font-bold"
               placeholder="new username" value={refreshUsername} onChange={e => setRefreshUsername(e.target.value)}
             />
 
@@ -171,16 +181,119 @@ const Profile = () => {
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-white mb-3">Change Your Password</h3>
-          <input
+
+      {/* hide/show new password */}
+      <div className="relative  container mx-auto mt-5">
+      <input
+        type={isPasswordVisible ? "text" : "password"}
+        placeholder="new password" value={refreshPassword} onChange={e => setRefreshPassword(e.target.value)}
+        className="w-full
+        outline-blue2 border-blue2 rounded-md p-2 bg-gray-100 border-2  placeholder-blue2 text-blue2 font-bold"
+      />
+      <button
+        className="absolute inset-y-0 right-0 flex items-center px-4 text-blue2 font-bold"
+        onClick={togglePasswordVisibility}
+      >
+        {isPasswordVisible ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        )}
+      </button>
+    </div>
+
+    {/* hide/show confirm new password */}
+    <div className="relative  container mx-auto mt-5">
+      <input
+        type={isConfirmPasswordVisible ? "text" : "password"}
+        placeholder="confirm new password" value={refreshConfirmPassword} onChange={e => setRefreshConfirmPassword(e.target.value)}
+        className="w-full
+        outline-blue2 border-blue2 rounded-md p-2 bg-gray-100 border-2  placeholder-blue2 text-blue2 font-bold"
+      />
+      <button
+        className="absolute inset-y-0 right-0 flex items-center px-4 text-blue2 font-bold"
+        onClick={toggleConfirmPasswordVisibility}
+      >
+        {isConfirmPasswordVisible ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        )}
+      </button>
+    </div>
+          {/* <input
               type="password"
               placeholder="new password" value={refreshPassword} onChange={e => setRefreshPassword(e.target.value)}
               className="w-72 mb-3 outline-blue2 border-blue2 rounded-md p-2 bg-gray-100 border-2  placeholder-blue2 text-blue2 font-bold"
-            />
-            <input
+            /> */}
+            {/* <input
               type="password"
               placeholder="confirm new password" value={refreshConfirmPassword} onChange={e => setRefreshConfirmPassword(e.target.value)}
               className="w-72 mb-2 outline-blue2 border-blue2 rounded-md p-2 bg-gray-100 border-2  placeholder-blue2 text-blue2 font-bold"
-            />
+            /> */}
+
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
